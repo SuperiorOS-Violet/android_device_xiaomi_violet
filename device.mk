67aeafa36a7ecc5018c2bd2f6e6b1ebc8e927bd0
Joker-V2 <nipinna0@gmail.com>
2020-12-05 06:35:10 +0000

    violet: Build XiaomiParts
    * Device specific settings
    
    This reverts commit 3f34faaecab459dc60b5d1f64c97ed9673984e0f.

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
